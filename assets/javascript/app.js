748e7a263c144bb1d02505e80357e44cab5ac57e
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-09-18 12:43:57 -0500

    Train-Schedule

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
 
+    //Initialize Firebase
     let config = {
-    apiKey: "AIzaSyCpJ9Hdlfy7jAsrEgxcvWRvOhJsdPj4pyk",
-    authDomain: "train-79fd1.firebaseapp.com",
-    databaseURL: "https://train-79fd1.firebaseio.com",
-    projectId: "train-79fd1",
-    storageBucket: "",
-    messagingSenderId: "397422251253"
+        apiKey: "AIzaSyCpJ9Hdlfy7jAsrEgxcvWRvOhJsdPj4pyk",
+        authDomain: "train-79fd1.firebaseapp.com",
+        databaseURL: "https://train-79fd1.firebaseio.com",
+        projectId: "train-79fd1",
+        storageBucket: "",
+        messagingSenderId: "397422251253"
     };
     firebase.initializeApp(config);
 
     let database = firebase.database();
 
+    //Add train on button click
     $("#addTrain").on("click", function (event) {
         event.preventDefault();
 
@@ -30,45 +32,40 @@ $(document).ready(function () {
 
     database.ref().on("child_added", function (childSnapshot) {
 
-        var newTrain = childSnapshot.val().trainName;
-        var newLocation = childSnapshot.val().destination;
-        var newFirstTrain = childSnapshot.val().firstTrain;
-        var newFreq = childSnapshot.val().frequency;
-    
-        // First Time (pushed back 1 year to make sure it comes before current time)
-        var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
-    
-        // Current Time
-        var currentTime = moment();
-    
-        // Difference between the times
-        var diffTime = moment().diff(moment(startTimeConverted), "minutes");
-    
-        // Time apart (remainder)
-        var tRemainder = diffTime % newFreq;
-    
-        // Minute(s) Until Train
-        var tMinutesTillTrain = newFreq - tRemainder;
-    
-        // Next Train
-        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-        var catchTrain = moment(nextTrain).format("HH:mm");
-    
+        let newTrain = childSnapshot.val().trainName;
+        let newLocation = childSnapshot.val().destination;
+        let newFirstTrain = childSnapshot.val().firstTrain;
+        let newFreq = childSnapshot.val().frequency;
+
+        //Setting up the time
+        let startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
+
+        let currentTime = moment();
+
+        let diffTime = moment().diff(moment(startTimeConverted), "minutes");
+
+        let tRemainder = diffTime % newFreq;
+
+        let tMinutesTillTrain = newFreq - tRemainder;
+
+        let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+        let catchTrain = moment(nextTrain).format("HH:mm");
+
         // Display On Page
         $("#display").append(
-          ' <tr><td>' + newTrain +
-          ' </td><td>' + newLocation +
-          ' </td><td>' + newFreq +
-          ' </td><td>' + catchTrain +
-          ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
-    
+            "<tr><td>" + newTrain +
+            "</td><td>" + newLocation +
+            "</td><td>" + newFreq +
+            "</td><td>" + catchTrain +
+            "</td><td>" + tMinutesTillTrain + "</td></tr>");
+
         // Clear input fields
         $("#trainName, #destination, #firstTrain, #interval").val("");
         return false;
-      },
+    },
         //Handle the errors
         function (errorObject) {
         });
-    
-    }); //end document ready
 
+}); 
